2723f549df90684c51966ac6a83d0970e1bbf9b8
Saad Muhamamd <s.muhammad86@gmail.com>
2020-10-17 03:05:04 +0500

    Final boiler plate with typescript & redux toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Hi
+        <Counter />
     </div>
   );
 }
